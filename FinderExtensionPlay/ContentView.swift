b4595e54f2ed57754d16064decb3c764d9d72b7a
Kamaal Farah <kamaal.f1@gmail.com>
2024-08-11 13:46:53 +0200

    Initial commit

---
+++ FinderExtensionPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinderExtensionPlay
+//
+//  Created by Kamaal M Farah on 11/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinderExtensionPlay/FinderExtensionPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderExtensionPlayApp.swift
+//  FinderExtensionPlay
+//
+//  Created by Kamaal M Farah on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FinderExtensionPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
