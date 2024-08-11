d7df8470c8ea2fc4eee02578ede5596a529a3613
Kamaal Farah <kamaal.f1@gmail.com>
2024-08-11 16:09:01 +0200

    Debugging with logs

--- FinderExtensionPlayAction/ActionViewController.swift
+++ FinderExtensionPlayAction/ActionViewController.swift
@@ -6,6 +6,9 @@
 //
 
 import Cocoa
+import OSLog
+
+private let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "ActionViewController")
 
 class ActionViewController: NSViewController {
 
@@ -16,26 +19,59 @@ class ActionViewController: NSViewController {
     }
 
     override func loadView() {
+        guard let context = extensionContext else { fatalError("Expected an extension context") }
+
         super.loadView()
-    
+
         // Insert code here to customize the view
-        NSLog("Input Items = %@", self.extensionContext!.inputItems as NSArray)
-    
-        let sharedItem = self.extensionContext!.inputItems[0] as! NSExtensionItem
+        let sharedItem = context.inputItems[0] as! NSExtensionItem
+        if let sharedItemText = sharedItem.attributedContentText?.string, !sharedItemText.isEmpty {
+            logger.info("Input string != EMPTY")
+        } else {
+            logger.info("Input string == EMPTY")
+        }
+
+        if context.inputItems.count > 1 {
+            logger.info("There are more than 1 input items")
+        } else {
+            logger.info("There is exactly 1 input item")
+        }
+
+        if let attachments = sharedItem.attachments {
+            if attachments.count > 1 {
+                logger.info("There are more than 1 atachments items")
+            } else if attachments.isEmpty {
+                logger.info("There are no atachments on this item")
+            } else {
+                let attachment = sharedItem.attachments![0]
+                attachment.loadInPlaceFileRepresentation(forTypeIdentifier: "Empty.txt") { url, loaded, error in
+                    if let error {
+                        logger.error("Error appeared")
+                        return
+                    }
+
+                    logger.info("Was able to load attachment")
+                }
+                logger.info("There is exactly 1 attachment item")
+            }
+        }
+
         let text = sharedItem.attributedContentText?.string
-        
-        if text != nil && !text!.isEmpty {
-            self.myTextView.string = text!
+
+        if let text, !text.isEmpty {
+            self.myTextView.string = text
         }
     }
 
     @IBAction func send(_ sender: AnyObject?) {
         guard let context = extensionContext else { fatalError("Expected an extension context") }
 
-        // Note: The extension information in the Info.plist is set to accept any type of content, but this example code only handles text. You should declare the specific types to be supported by your extension in the extension's Info.plist and then make sure to handle all your supported types.
+        // Note: The extension information in the Info.plist is set to accept any type of content,
+        // but this example code only handles text. You should declare the specific types to be supported by your
+        // extension in the extension's Info.plist and then make sure to handle all your supported types.
         let inputFile = context.inputItems[0] as! NSExtensionItem
-//        inputFile.attributedContentText = self.myTextView.attributedString()
-    
+        inputFile.attributedContentText = self.myTextView.attributedString()
+
         let outputItems = [inputFile]
         context.completeRequest(returningItems: outputItems, completionHandler: nil)
     }
